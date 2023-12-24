347b3026711ba5230cc7db7dec9b4fb578ed7d66
asadujaman74 <asadujaman11@gmail.com>
2023-12-24 22:31:53 +0600

    student registration added

--- mainAcademy/iacademy/views.py
+++ mainAcademy/iacademy/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def firstview(request):
     return HttpResponse('Hello World')
+
+
+def register_student(request):
+    return render(request, 'register_student.html')
